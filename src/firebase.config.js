26275b9291eb2a02a3fc36747de872d36cdf65e5
Daniel <daniel.kapexhiu@gmail.com>
2019-12-06 10:55:09 +0100

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyCLE6QWUmvltRQ86hjBHfOKMDYobTDx7e8",
-  authDomain: "product-management-71225.firebaseapp.com",
-  databaseURL: "https://product-management-71225.firebaseio.com",
-  projectId: "product-management-71225",
-  storageBucket: "product-management-71225.appspot.com",
-  messagingSenderId: "972362764328",
-  appId: "1:972362764328:web:c0d81b320546701967e51d"
+  apiKey: "xxx",
+  authDomain: "xxx",
+  databaseURL: "xxx",
+  projectId: "xxx",
+  storageBucket: "xxx",
+  messagingSenderId: "xxx",
+  appId: "xxx"
   };
 
 export default firebase.initializeApp(config);
